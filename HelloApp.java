8aa3d22c2d85b389d86131fe985b465216212bf0
amrita-s2521 <as2521@srmist.edu.in>
2026-04-01 22:52:26 +0530

    Add uc4

--- HelloApp.java
+++ HelloApp.java
@@ -1,11 +1,16 @@
 package com.srm.src;
-
 public class HelloApp {
-	public static void main(String[] args) {
-		String name = "World";
-		if (args.length > 0) {
-			name = args[0]; 
-		}
-		System.out.println("Hello, "+name + "!");
-		}
+    public static void main(String[] args) {
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+
+            if (i < args.length - 1) {
+                nameBuilder.append(", ");
+            }
+        }
+        String name = nameBuilder.toString();
+        System.out.println("Hello " + name + "!");
+    }
 }
