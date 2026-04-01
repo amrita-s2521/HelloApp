e7bfb543ce7c89fefbe6c06e092ec522cb2a5e69
amrita-s2521 <as2521@srmist.edu.in>
2026-04-01 22:57:29 +0530

    Add uc5

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,16 @@
 package com.srm.src;
+
 public class HelloApp {
     public static void main(String[] args) {
         StringBuilder nameBuilder = new StringBuilder();
-
-        for (int i = 0; i < args.length; i++) {
-            nameBuilder.append(args[i]);
-
-            if (i < args.length - 1) {
+        boolean first = true;
+        for (String name : args) {
+            if (!first) {
                 nameBuilder.append(", ");
             }
+            nameBuilder.append(name);
+            first = false;
         }
-        String name = nameBuilder.toString();
-        System.out.println("Hello " + name + "!");
+        System.out.println("Hello, " + nameBuilder.toString() + "!");
     }
 }
